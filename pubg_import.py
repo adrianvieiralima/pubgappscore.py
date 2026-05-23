32dd17be61b642d2698fbd1ae040ed3c8fadfee7
Adriano Vieira <adrianovieiradelima@gmail.com>
2026-05-23 18:22:08 -0300

    Update pubg_import.py

--- pubg_import.py
+++ pubg_import.py
@@ -19,7 +19,7 @@ players = [
     "Adrian-Wan", "MironoteuCool", "FabioEspeto", "Mamutag_Komander",
     "Robson_Foz", "MEIRAA", "EL-LOCORJ", "SalaminhoKBD",
     "nelio_ponto_dev", "CARNEIROOO", "Kowalski_PR", "Zacouteguy",
-    "Sidors", "Takato_Matsuki", "cmm01", "Petrala", "O-CARRASCO", "DET4N4KA", "LeandroTW2"
+    "Sidors", "Takato_Matsuki", "cmm01", "Petrala", "O-CARRASCO", "DET4N4KA", "LeandroTW2", "HELI_NERES"
 ]
 
 def fazer_requisicao(url):
